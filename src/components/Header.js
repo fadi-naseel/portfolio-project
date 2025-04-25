ba44714502664c912ba9d036be4d282c75165015
Fadi Naseel <nusaibapc@gmail.com>
2025-04-25 14:24:48 +0530

    initial

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="logo">Portfolio</h1>
+      <h1 className="logo">Fadi Naseel</h1>
       <nav className="nav">
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/contact" className="nav-link">Contact</Link>
